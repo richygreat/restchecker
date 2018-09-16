0310326725b47b93a5da054d209f555f3d447061
Richy Great <richygreat@gmail.com>
2018-09-17 00:53:30 +0200

    Some Tweaks

--- src/main/java/com/rg/test/restchecker/RestCheckerApplication.java
+++ src/main/java/com/rg/test/restchecker/RestCheckerApplication.java
@@ -7,8 +7,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class MarketoApplication {
+public class RestCheckerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(MarketoApplication.class, args);
+		SpringApplication.run(RestCheckerApplication.class, args);
 	}
 }
--- src/main/java/com/rg/test/restchecker/controller/TestRestController.java
+++ src/main/java/com/rg/test/restchecker/controller/TestRestController.java
@@ -1,6 +1,7 @@
 package com.rg.test.restchecker.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,18 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 public class TestRestController {
+	private String lastRequest;
+
 	@RequestMapping("hitback")
 	public ResponseEntity<String> hitBack(@RequestBody String body) {
 		log.info(body);
+		lastRequest = body;
 		return ResponseEntity.ok(body);
+	}
+
+	@GetMapping("last")
+	public ResponseEntity<String> getLastRequest() {
+		log.info(lastRequest);
+		return ResponseEntity.ok(lastRequest);
 	}
 }
